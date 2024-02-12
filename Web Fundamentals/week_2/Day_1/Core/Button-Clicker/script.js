57af4b519667f7270d2f3d34b2304b67ba72f998
hamzajbali <hamzajbali96@gmail.com>
2024-02-12 15:01:09 +0100

    Button-Clicker

---
+++ Web Fundamentals/week_2/Day_1/Core/Button-Clicker/script.js
@@ -0,0 +1,13 @@
+let btn = document.querySelector('.nav-btn');
+
+function change() {
+    if(btn.innerText=="Login"){
+        btn.innerText="Logout";
+    } else {
+        btn.innerText="Login";
+    }
+ 
+}
+function hide(element) {
+    element.remove();
+}
